07295373bf70677cf865b7a0778a93e7f89b638e
Adam Turner <aturner237@gmail.com>
2024-10-11 20:43:10 +0100

    added script on how to create new repo

--- test_script.R
+++ test_script.R
@@ -1,1 +1,8 @@
 ##testing git again :)
+#use R to create a new github repo
+install.packages("usethis")
+library(usethis)
+create_github_token()
+gitcreds::gitcreds_set()
+library(usethis)
+use_github()
